5680f47ba2a89b8bfe3569d20334c3d3503fc904
Johan Nawawi <145430844+johannawawi@users.noreply.github.com>
2025-05-16 09:04:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ st.markdown("""
         z-index: 100;
     }
     .small-download button {
-        font-size: 14px !important;
+        font-size: 10px !important;
         padding: 8px 12px !important;
         font-family: 'Inter', sans-serif !important;
     }
